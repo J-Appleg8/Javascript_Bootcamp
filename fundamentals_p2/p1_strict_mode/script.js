064ce8846f1bc8aab08a2bc33185913fb1d159b5
J-Appleg8 <84997748+J-Appleg8@users.noreply.github.com>
2022-02-27 16:48:32 -0800

    added strict statement to all js scripts - finished first functions lesson

--- fundamentals_p2/p1_strict_mode/script.js
+++ fundamentals_p2/p1_strict_mode/script.js
@@ -5,3 +5,14 @@
 ////////////////////
 //// Overview /////
 //////////////////
+//
+// Has to be the very first statement in the script
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// Without strict mode, the console just shows nothing because
+// hasDriversLicense wasnt actually set to true
+if (passTest) hasDriverLicense = true;
+if (hasDriversLicense) console.log("I can drive");
--- fundamentals_p2/p2_functions/script.js
+++ fundamentals_p2/p2_functions/script.js
@@ -5,3 +5,23 @@
 ////////////////////
 //// Overview /////
 //////////////////
+//
+// Functions should be used following the DRY principle - Dont Repeat Yourself
+"use strict";
+
+function logger() {
+  console.log("My name is James");
+}
+
+logger();
+
+// Function with parameters
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+
+console.log(appleJuice);
